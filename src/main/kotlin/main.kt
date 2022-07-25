98c9f375b679214b7dc12971d98566c3ebe3b038
ryg-git <ygretharekar@outlook.com>
2022-07-25 22:38:00 +0530

    CF 1702B - Polycarp Writes a String from Memory

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,26 @@
 fun next() = readLine()!!.trim()
 
 fun main() {
-    var inp = next().toInt()
+    var t = next().toInt()
 
-    while(inp != 42) {
-        println(inp)
-        inp = next().toInt()
+    while(t-- > 0) {
+        solve()
     }
+}
+
+fun solve() {
+    val n = next().toCharArray()
+    var ans = charArrayOf()
+    var an = 0
+    for (a in n) {
+        if(ans.size == 3 && a !in ans) {
+            ans = charArrayOf()
+            an++
+        }
+        if (a !in ans) {
+            ans += a
+        }
+    }
+    an += if (ans.isEmpty()) 0 else 1
+    println(an)
 }
