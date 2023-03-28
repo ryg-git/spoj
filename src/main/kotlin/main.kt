a98432c07cb72e53d4c5c6fb827c59b6787d090e
Yashawant Retharekar <ygretharekar@outlook.com>
2023-03-28 12:02:12 +0530

    SPOJ PALIN - The Next Palindrome

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,3 @@
-import java.util.*
-
-
 fun next() = readLine()!!.trim()
 fun nextInt() = readLine()!!.trim().toInt()
 fun nextInts() = readLine()!!.trim().split(" ").map { it.toInt() }
@@ -14,16 +11,58 @@ fun main() {
 }
 
 fun solve() {
-    val expr = next()
-    val st = ArrayDeque<Char>(listOf())
-    for (i in expr.toCharArray()) {
-        if (i == ')')  {
-            val tp = st.removeLast()
-            st.removeLast()
-            print(tp)
+    val str = next().toCharArray().dropWhile { it == '0' }.toMutableList()
+
+    val half = str.size / 2
+
+    var ne = true
+
+    var ans = mutableListOf<Char>()
+
+    var isSmaller = false
+    var isLarger = false
+    var isEqual = true
+
+    for (i in half..str.lastIndex) {
+        if (!isLarger && str[i] > str[str.lastIndex - i]) {
+            isSmaller = true
+            isEqual = false
         }
-        else if (i.isLetter()) print(i)
-        else st.addLast(i)
+        if (!isSmaller && str[i] < str[str.lastIndex - i]) {
+            isLarger = true
+            isEqual = false
+        }
+
+        ne = ne && str[i] == str[str.lastIndex - i]
+
+        str[i] = str[str.lastIndex - i]
     }
-    println()
+
+    if (isSmaller || isEqual) {
+        var changed = false
+
+        for (i in half..str.lastIndex) {
+            if (str[i] != '9') {
+                val newNum = (str[i].toInt() + 1).toChar()
+                str[i] = newNum
+                str[str.lastIndex - i] = newNum
+                changed = true
+                ans = str.toMutableList()
+                break
+            } else {
+                str[i] = '0'
+                str[str.lastIndex - i] = '0'
+            }
+        }
+
+        if (!changed) {
+            ans.add('1')
+            for (i in 1..str.lastIndex) ans.add('0')
+            ans.add('1')
+        }
+    } else {
+        ans = str.toMutableList()
+    }
+
+    println(ans.joinToString(""))
 }
