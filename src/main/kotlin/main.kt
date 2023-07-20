1ce0d9df7833c874e0a1502957fa0a4091d2bde3
ryg-git <ygretharekar@outlook.com>
2023-07-20 20:44:00 +0530

    LC Streak 20230720

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -27,68 +27,50 @@ fun main() {
     }
 }
 
-class ListNode(var `val`: Int) {
-    var next: ListNode? = null
-}
-
 fun solve() {
-    val l1 = ListNode(9).apply { next = ListNode(4) }
-    val l2 = ListNode(9)
+    val a = intArrayOf(-2, 2, 1, -2)
 
-    val ans = addTwoNumbers(l1, l2)
-    println(ans)
+    val ans = asteroidCollision(a)
+
+    for (i in ans) print("$i, ")
+    println()
 }
 
-fun lastNodes(ans: MutableList<Int>, l1: ListNode?, l2: ListNode?) {
-    if (l1!!.next != null) {
-        if (l2!!.next == null) {
-            return
+fun asteroidCollision(asteroids: IntArray): IntArray {
+    val st = ArrayDeque<Int>()
+    val neg = mutableListOf<Int>()
+    for (i in asteroids) {
+        if (i > 0) {
+            st.addLast(i)
+        } else {
+            if (st.isNotEmpty()) {
+                var e = st.last()
+                if (e + i > 0) {
+                    continue
+                } else if (e + i == 0) {
+                    st.removeLast()
+                } else if (e + i < 0) {
+                    var equalFlag = false
+                    while (e + i <= 0 && st.size > 0) {
+                        st.removeLast()
+                        if (e == (-1 * i)) {
+                            equalFlag = true
+                            break
+                        }
+                        if (st.isNotEmpty()) e = st.last()
+                    }
+                    if (!equalFlag && st.size == 0) neg.add(i)
+                }
+            } else {
+                neg.add(i)
+            }
         }
     }
+
+    for (i in st) neg.add(i)
+
+    return neg.toIntArray()
 }
 
-fun getNumInString(num: ListNode?): String {
-    return if (num == null) ""
-    else num.`val`.toString() + getNumInString(num.next)
-}
 
-fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
-    var sl1 = getNumInString(l1)
-    var sl2 = getNumInString(l2)
 
-    if (sl1.length > sl2.length) sl2 = sl2.padStart(sl1.length, '0')
-    else sl1 = sl1.padStart(sl2.length, '0')
-
-//    println(sl1)
-//    println(sl2)
-
-    var carry = 0
-
-    var an = ""
-
-    for (i in sl1.lastIndex downTo 0) {
-        val s = carry + sl1[i].digitToIntOrNull()!! + sl2[i].digitToIntOrNull()!!
-
-        val re = s % 10
-        carry = s / 10
-
-        an += re
-    }
-
-    if(carry > 0) an += carry
-
-//    println(an.reversed())
-
-    var fir: ListNode? = null
-
-    for (i in an) {
-        fir = ListNode(i.digitToInt()).apply { next = fir }
-    }
-
-//    while (fir != null) {
-//        print(fir.`val`)
-//        fir = fir.next
-//    }
-
-    return fir
-}
