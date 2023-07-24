969269a70b0260d87225bd8fed4d47447f84ea20
ryg-git <ygretharekar@outlook.com>
2023-07-24 20:46:04 +0530

    LC Streak 20230724

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -21,38 +21,18 @@ fun main() {
 fun solve() {
 //    val nums = intArrayOf(1, 2, 4, 3, 5, 4, 7, 2)
 //    val nums = intArrayOf(1, 3, 5, 4, 7)
-    val ans = allPossibleFBT(3)
+    val ans = myPow(3.0, 3)
 
     println(ans)
 }
 
-class TreeNode(var `val`: Int) {
-    var left: TreeNode? = null
-    var right: TreeNode? = null
+fun lxPow(x: Double, n: Long): Double {
+    if (n == 0L) return 1.0
+    if (n == 1L) return x
+    if (n < 0) return 1.0 / lxPow(x, -1L * n)
+    return if (n % 2 == 0L) lxPow(x * x, n / 2) else x * lxPow(x * x, (n - 1) / 2)
 }
 
-fun allPossibleFBT(n: Int): List<TreeNode?> {
-    if (n % 2 == 0) {
-        return listOf()
-    }
-
-    val dp = Array(n + 1) { mutableListOf<TreeNode?>() }
-
-    dp[1] = mutableListOf(TreeNode(0))
-
-    for (cnt in 3..n step 2) {
-        for (i in 1 until cnt step 2) {
-            val j = cnt - i - 1
-            for (left in dp[i]) {
-                for (right in dp[j]) {
-                    val tn = TreeNode(0)
-                    tn.left = left
-                    tn.right = right
-                    dp[cnt].add(tn)
-                }
-            }
-        }
-    }
-
-    return dp[n]
+fun myPow(x: Double, n: Int): Double {
+    return lxPow(x, n.toLong())
 }
